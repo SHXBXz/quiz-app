82b2da0f5acb3806868ab9b1034c6c510194e5aa
SHXBXz <natdanai.nuya2546@gmail.com>
2025-09-01 10:13:20 +0700

    button

--- src/component/Quiz.js
+++ src/component/Quiz.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+import QuestionsData from "../data/QuestionsData";
+
 const Quiz = ()=>{
+    //console.log(QuestionsData);
+    const [current,setCurrent]= useState(0)
+    const [selectChoice,setSelectChoice] = useState("")
+    
     return(
         <div className="quiz">
             <h1>Quiz Component</h1>
+            <div className="choices">
+                <button onClick={()=>setSelectChoice("A")}>{QuestionsData[current].A}</button>
+                <button onClick={()=>setSelectChoice("B")}>{QuestionsData[current].B}</button>
+                <button onClick={()=>setSelectChoice("C")}>{QuestionsData[current].C}</button>
+                <button onClick={()=>setSelectChoice("D")}>{QuestionsData[current].D}</button>
+            </div>
+            <p>{`${current+1}/${QuestionsData.length}`}</p>
         </div>
     )
 }
