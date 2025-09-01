471008d99e50d98595fa26bf93e256ffaf20a771
SHXBXz <natdanai.nuya2546@gmail.com>
2025-09-01 10:32:33 +0700

    Nice

--- src/component/Quiz.js
+++ src/component/Quiz.js
@@ -1,14 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState,useContext } from "react";
 import QuestionsData from "../data/QuestionsData";
+import { DataContext } from "../App";
 
 const Quiz = ()=>{
     //console.log(QuestionsData);
     const [current,setCurrent]= useState(0)
     const [selectChoice,setSelectChoice] = useState("")
+    const {score,setScore,setAppState} = useContext(DataContext)
     
+
+    useEffect (()=>{
+        checkAnswer()
+    },[selectChoice])
+
+    const checkAnswer=()=>{
+        if(selectChoice!==""){
+            if(selectChoice===QuestionsData[current].answer){
+                setScore(score+1)
+                console.log("ตอบถูกและได้คะแนน")
+                nextQuestion()
+            }else{
+                console.log("ตอบผิดไม่ได้คะแนน ToT")
+                nextQuestion()
+            }
+        }
+    }
+    const nextQuestion=()=> {
+        setSelectChoice("")
+        if(current===QuestionsData.length-1){
+            setAppState("score")
+        }else{
+            setCurrent(current+1)
+        }
+        }
+
     return(
         <div className="quiz">
-            <h1>Quiz Component</h1>
+            <h1>{QuestionsData[current].question}</h1>
             <div className="choices">
                 <button onClick={()=>setSelectChoice("A")}>{QuestionsData[current].A}</button>
                 <button onClick={()=>setSelectChoice("B")}>{QuestionsData[current].B}</button>
